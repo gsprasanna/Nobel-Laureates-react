9c84278e8fa172540df4e504c175df6fd7d898eb
Prasanna <prasanna@Prasannas-MacBook-Pro.local>
2019-11-08 18:30:30 +0530

    added header, footer components

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="page-footer font-small blue">
+      <div className="footer-copyright text-center py-3">
+        Made with &#10084; by Prasanna Balaji.
+        <a href="https://github.com/gsprasanna/"> Github</a>
+        <a href="https://twitter.com/Prasanna_GS_"> Twitter</a>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import NobelPrizeLogo from "../assets/images/NobelPrizeIcon.jpeg";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="info" variant="dark" sticky="top">
+        <Navbar.Brand href="https://gsprasanna.github.io/">
+          <img
+            alt="logo"
+            src={NobelPrizeLogo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          {"THE NOBEL PRIZE"}
+        </Navbar.Brand>
+      </Navbar>
+    </>
+  );
+};
+export default Header;
