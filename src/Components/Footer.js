072a79f2f5423670f719ee2a01895204fcbb5be8
Prasanna <prasanna@Prasannas-MacBook-Pro.local>
2019-11-08 18:45:20 +0530

    added my social medial profile links in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,8 +5,9 @@ const Footer = () => {
     <footer className="page-footer font-small blue">
       <div className="footer-copyright text-center py-3">
         Made with &#10084; by Prasanna Balaji.
-        <a href="https://github.com/gsprasanna/"> Github</a>
+        <a href="https://github.com/gsprasanna/Nobel-Laureates-App"> Github</a>
         <a href="https://twitter.com/Prasanna_GS_"> Twitter</a>
+        <a href="https://linkedin.com/in/prasanna-balaji-gs"> LinkedIn</a>
       </div>
     </footer>
   );
